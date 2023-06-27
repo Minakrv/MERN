bfb4d1bd3f577b7e00308a9826e92fb77f36ba6b
Mina Khosravi <minsmscbook@Minas-MacBook-Air.local>
2023-06-27 01:10:24 +0100

    working o showing the data on landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,38 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
 import express, { Request, Response } from "express";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import cors from "cors";
 import Deck from "./models/Deck";
 
+const PORT = 8000;
 const app = express();
-const PORT = 3000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
-app.post('/decks', async (req: Request, res: Response) => {
-    const newDeck = new Deck({
-        title: req.body.title,
-    });
-    const cretedDeck = await newDeck.save();
-    res.json(cretedDeck)
-})
+app.get("/decks", async (req: Request, res: Response) => {
+
+   const decks = await Deck.find();
+   console.log(decks);
+
+   res.json(decks);
+});
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const cretedDeck = await newDeck.save();
+  res.json(cretedDeck);
+});
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
-    console.log(`Listening to ${PORT}`);
-    app.listen(PORT);
-})
+  console.log(`Listening to ${PORT}`);
+  app.listen(PORT);
+});
